cbbc0dc61623b910cab657f5afd13c2a8d493d03
JeanneLG <jeanne.le-guillou@insa-lyon.fr>
2025-04-08 16:12:57 +0200

    Add files via upload

--- link.py
+++ link.py
@@ -8,18 +8,45 @@ from keras.models import load_model
 from algo_gen import crossover, mutation
 from Interface_graphique import RobotPortrait, root
 
+
+def newImages(imgOg):
+    V, _, _ = encoder.predict(imgOg, batch_size=32)
+    
+    #algo gen
+    crossFace1,crossFace2,crossFace3,crossFace4,crossFace5,crossFace6 = crossover(V[0], V[1], V[2], V[3])
+
+    mutant1=mutation(crossFace1)
+    mutant2=mutation(crossFace2)
+    mutant3=mutation(crossFace3)
+    mutant4=mutation(crossFace4)
+    mutant5=mutation(crossFace5)
+    mutant6=mutation(crossFace6)
+
+    mutant7=mutation(crossFace1)
+    mutant8=mutation(crossFace2)
+    mutant9=mutation(crossFace3)
+    mutant10=mutation(crossFace4)
+    mutant11=mutation(crossFace5)
+    mutant12=mutation(crossFace6)
+    
+    mutatedV=np.array([mutant1,mutant2,mutant3,mutant4,mutant5,mutant6, mutant7,mutant8, mutant9, mutant10, mutant11, mutant12])
+    
+    nouvelles_images = decoder.predict([mutatedV])
+    return nouvelles_images
+
 # récupération du modèle
 encoder = load_model("encoder_model.keras", compile=False)
 decoder = load_model("decoder_model.keras", compile=False)
 
 
 #génération d'un vecteur latent
-images_originales= RobotPortrait(root).select_portrait() # il faudra mettre le choix de l'user
+#images_originales= RobotPortrait(root).select_portrait() # il faudra mettre le choix de l'user
 
 #calcul vecteurs latents
-V, _, _ = encoder.predict(images_originales, batch_size=32)
+#V, _, _ = encoder.predict(images_originales, batch_size=32)
 
 ### rentre en jeu l'algo gen
+'''
 crossFace1,crossFace2,crossFace3,crossFace4,crossFace5,crossFace6 = crossover(V[0], V[1], V[2], V[3])
 
 mutant1=mutation(crossFace1)
@@ -35,15 +62,14 @@ mutant9=mutation(crossFace3)
 mutant10=mutation(crossFace4)
 mutant11=mutation(crossFace5)
 mutant12=mutation(crossFace6)
-
-mutatedV=np.array([mutant1,mutant2,mutant3,mutant4,mutant5,mutant6,
-                  mutant7,mutant8, mutant9, mutant10, mutant11, mutant12])
+'''
+#mutatedV=np.array([mutant1,mutant2,mutant3,mutant4,mutant5,mutant6, mutant7,mutant8, mutant9, mutant10, mutant11, mutant12])
 
 # reconstruction nouvelles images avec vecteurs latents mutés
-nouvelles_images = decoder.predict([mutatedV])
+#nouvelles_images = decoder.predict([mutatedV])
 
 #affichage à envoyer dans l'interface
-
+'''
 plt.subplot(2, 2, 2)
 plt.title("Nouveau portrait 1")
 plt.imshow(nouvelles_images[0])
@@ -121,3 +147,4 @@ plt.title("Nouveau portrait 2")
 plt.imshow(nouvelles_images[12])
 plt.axis('off')
 plt.show()
+'''
