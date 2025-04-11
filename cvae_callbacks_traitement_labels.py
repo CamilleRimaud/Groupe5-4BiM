d2d5126a99a418128b0c7711fe5225aad8efd628
mangoo8 <degabriel2565@gmail.com>
2025-04-11 16:37:17 +0200

    Final version of cvae
    data import, encoder, decoder, cvae, model fitting+saving+loading, unit test

--- cvae_callbacks_traitement_labels.py
+++ cvae_callbacks_traitement_labels.py
@@ -18,6 +18,7 @@ import tensorflow as tf
 import keras
 import random
 from keras import ops
+from keras import Model, Input
 from keras import layers, models
 from keras import backend as K
 from keras.callbacks import TensorBoard
@@ -29,138 +30,45 @@ from sklearn.model_selection import train_test_split
 from google.colab import drive
 drive.mount('/content/drive')
 
-cd drive/MyDrive/Colab Notebooks/data_sample_10000
+cd drive/MyDrive/Colab Notebooks
 """
-def format_img(id):
-    '''
 
-    Parameters
-    ----------
-    The identifier of the image to load
+# Load compressed data
+loaded = np.load("processed_faces_50000.npz") # Check the npz filename and path
+data = loaded["data"]
 
+# Normalization
+data = data.astype('float32') / 255.0
+print("Data shape: ", data.shape)  # The shape should be (50000, 128, 128, 3)
 
-    Returns
-    -------
-    The image cropped and resized in a numpy array of shape (128, 128, 3)
-
-    '''
-     # Load the image and turn it into a numpy array
-    image = Image.open(f'./data_sample/{id}.jpg')
-
-    """
-    #Size of the original image
-    width, height = image.size
-
-    # Normalization of the image
-    # The image is centered and cropped to widthxwidth format
-    left = 0
-    top = (height/2)-(width/2)
-    right = width
-    bottom = (height/2)+(width/2)
-    """
-
-    image = image.crop((0, 20, 178, 198))
-
-    # Change of resolution to 128x128
-    img_resized = image.resize((128, 128))
-
-    # -1 to flatten, 3 for RGB channels
-    image_np = np.array(img_resized) # do not flatten
-
-    return image_np
-
-def import_data(nb_img):
-  data=[]
-
-  for i in range(1, nb_img+1):
-      id = f"{i:06}"  # Format the ID with 5 numbers, filled with zeroes on the left side
-      data.append(format_img(id))
-      if i%100==0:
-          print(id)
-
-
-  data=np.array(data)
-  print(data.shape)
-
-  # Normalization of the pixels
-  data=data/255
-
-  return data
-"""
-nb_img=500
-data=import_data(nb_img)
-
-import json
-
-with open("data.json", 'w') as f:
-    # indent=2 is not needed but makes the file human-readable
-    # if the data is nested
-    json.dump(data, f, indent=2)
-
-with open("data.json", 'r') as f:
-    data = json.load(f)
-
-print(data.shape)
-
-def select_attribute(my_attribute): # à supprimer
-  # Allows you to choose an attribute by name
-  attributes_names = np.genfromtxt("list_attr_celeba.txt", dtype=str, skip_header=1, max_rows=1)
-
-  index = np.where(attributes_names == f"{my_attribute}")[0]  # Return index table
-  my_index=index[0]+1
-  print(my_index)
-  return my_index
-
-my_attribute='Male' # à supprimer
-my_index=select_attribute(my_attribute)
-
-def get_corresponding_data(nb_img, my_index): # à supprimer
-  attributes = np.loadtxt("list_attr_celeba.txt", skiprows=2, dtype='str')
-  attributes_short = attributes[0:nb_img]
-  genre = np.array(attributes_short[:,my_index], dtype=int)
-  unique, counts = np.unique(genre, return_counts=True)
-  unique = map(str, unique)
-  counts = map(int, counts)
-  headcounts=dict(zip(unique, counts))
-
-  # Allows you to see if the distribution of the attribute is balanced
-  print(f"{my_attribute} repartition {headcounts}")
-  return genre
-
-genre=get_corresponding_data(nb_img, my_index) # à supprimer
-"""
-from keras import Model, Input
 
 def build_encoder(input_shape=(128, 128, 3)):
     '''
-
     Parameters
     ----------
       - The image of shape (128,128,3)
 
-    Details
-    -------
+    Description
+    -----------
     The encoder (CNN) is structured as followed:
     1. Treatment of the image: extracting the image features
       - 2 Conv2D layers using 32 filters of size 3x3, with a stride of 1 to extract basic characteristics from each pixel of the image.
       - 1 MaxPooling layer using a filter of size 2x2, with a stride of 2 to reduce the size of feature map while preserving the most important information.
-      - 2 more 2 Conv2D layers of 64 and 128 filters, followed alternately by MaxPooling layers
+      - 2 more Conv2D layers of 64 and 128 filters, followed alternately by MaxPooling layers
       - 1 flatten layer to flatten the multidimensional data into a 1D vector.
       - 1 dense (or fully connected) layer with 512 neurons.
 
     2. The variational part:
       - 2 dense layers perform a linear transformation to combine the 512 values and project them into a latent_dim dimension space using weight and bias.
-      1 layer generate the mean of the latent distribution, the other generate the log-variance
+      - 1 layer generate the mean of the latent distribution, the other generate the log-variance
       - 1 Lambda layer is used to limit the values of log-variance in the range [-10.0, 10.0] and to prevent them from becoming too large or too small,
-      which could affect model stability during training.
+        which could affect model stability during training.
 
     Then it use the function Sampling() to create a sample from the latent distribution using the previous mean, log_variance and an additionl noise.
 
     Returns
     -------
     A keras Model able to encode an image of shape (128,128,3) into a latent vector of size latent_dim
-
-
     '''
     img_input = Input(shape=input_shape, name="encoder_image_input")
 
@@ -197,13 +105,12 @@ def build_encoder(input_shape=(128, 128, 3)):
 
 def build_decoder(output_shape=(128, 128, 3)):
     '''
-
     Parameters
     ----------
       - The latent vector of size latent_dim
 
-    Details
-    -------
+    Description
+    -----------
     The decoder is structured as followed:
       - 1 dense layer to extract information from the latent vector into a 1D vector of size 16 384 (8*8*256)
       - 1 reshape layer to reshape the vector to the shape (8, 8, 256). This corresponds to a "features map" of size 8x8 with 256 channels.
@@ -212,11 +119,9 @@ def build_decoder(output_shape=(128, 128, 3)):
       - One last transposed convolution layer generates the output image. It uses 3 filters, which corresponds the 3 channels of a RGB image.
       The sigmoid activation function is used here because it produces values between 0 and 1, to keep a normalized image.
 
-
     Returns
     -------
     A keras Model able to decode a latent vector of size latent_dim into an image of shape (128, 128, 3)
-
     '''
     latent_input = Input(shape=(latent_dim,), name="decoder_latent_input")
 
@@ -237,7 +142,6 @@ def build_decoder(output_shape=(128, 128, 3)):
 class CVAE(keras.Model):
     def __init__(self, encoder, decoder, variational=False, **kwargs):
         '''
-
         Parameters
         ----------
           - The encoder and decoder models that the CVAE model will use to encode the input data and reconstruct it.
@@ -245,11 +149,9 @@ class CVAE(keras.Model):
             If variational=True, KL loss will be calculated in addition to the reconstruction loss.
           - **kwargs: allows to pass other arguments (such as data) to the parent class constructor keras. Model.
 
-
         Returns
         -------
         The image cropped and resized in a numpy array of shape (128, 128, 3)
-
         '''
         super(CVAE, self).__init__(**kwargs)
         self.encoder = encoder
@@ -257,15 +159,12 @@ class CVAE(keras.Model):
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
 
-
        # Initialization of the attribute 'variational' to control KL loss
         self.variational = variational
         if self.variational:
             self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
         else:
             self.kl_loss_tracker = None  # No KL loss if 'variational' is False
-
-
 
     @property
     def metrics(self):
@@ -303,8 +202,6 @@ class CVAE(keras.Model):
             else:
                 total_loss = reconstruction_loss
 
-
-
         # Gradients computation and weights update
         #  The gradients of the total loss versus the model weights are calculated and used to update the weights via the optimizer.
         grads = tape.gradient(total_loss, self.trainable_weights)
@@ -323,6 +220,15 @@ class CVAE(keras.Model):
 
 # Save of loss functions
 class LossHistoryCallback(keras.callbacks.Callback):
+    '''
+    Description
+    -----------
+    Callback that records loss values at the end of each epoch during training.
+
+    It stores the total loss, reconstruction loss, and KL divergence loss in 
+    three separate lists. These values can later be used to visualize training 
+    progress and assess whether the model is fitting well.
+    '''
     def __init__(self):
         super().__init__()
 
@@ -334,6 +240,15 @@ class LossHistoryCallback(keras.callbacks.Callback):
 
 # Generation of images at every 10 epochs
 class GenerateImageCallback(tf.keras.callbacks.Callback):
+    '''
+    Description
+    -----------
+    Callback that generates and displays two images every N epochs during training.
+
+    One image is generated from a random latent vector, and the other is reconstructed
+    from a real image passed through the encoder. This allows visual monitoring of
+    both the decoder's generative ability and reconstruction quality over time.
+    '''
     def __init__(self, encoder, decoder, latent_dim, epoch_interval=10):
         self.encoder = encoder
         self.decoder = decoder
@@ -367,16 +282,17 @@ class GenerateImageCallback(tf.keras.callbacks.Callback):
             plt.show()
 
 latent_dim = 32  #  latent space dimension
-"""
+
+# Build the first encoder/decoder
 encoder = build_encoder(input_shape=(128, 128, 3))
 decoder = build_decoder(output_shape=(128, 128, 3))
 encoder.summary()
-
 decoder.summary()
 
+# Split the data into training and testing parts
 x_train, x_test = train_test_split(data, test_size=0.2, random_state=42)
 
-# Saving loss
+# Create lists which contain loss values
 total_loss_history = []
 reconstruction_loss_history = []
 kl_loss_history = []
@@ -393,8 +309,6 @@ cvae.compile(optimizer=keras.optimizers.Adam(clipnorm=1.0))
 cvae.fit(x_train, epochs=100, batch_size=32, callbacks=[history_callback, gen_callback])
 
 # Plot the loss as graphic
-# import matplotlib.pyplot as plt
-
 plt.plot(total_loss_history, label="Total Loss")
 plt.plot(reconstruction_loss_history, label="Reconstruction Loss")
 plt.plot(kl_loss_history, label="KL Loss")
@@ -404,19 +318,19 @@ plt.legend()
 plt.title("CVAE Loss over Epochs")
 plt.show()
 
-total loss, reconstruction loss et KL loss convergent rapidement et sont stables.
+# According to the graphic, check if total loss, reconstruction loss et KL loss convergent rapidement et sont stables.
 
-# Save the model for future usage (encoder, decoder)
-encoder.save_weights("encoder.weights.h5")
-decoder.save_weights("decoder.weights.h5")
+# Save the model for future usage (encoder, decoder trained with 50000 images)
+encoder.save_weights("encoder_50000.weights.h5")
+decoder.save_weights("decoder_50000.weights.h5")
 
-# Build 2 new encoder/decoder
+# Build 2 new encoder/decoder for testing the saved models
 enc = build_encoder(input_shape=(128,128,3))
 dec = build_decoder(output_shape=(128,128,3))
 
 # Add weights to the models
-enc.load_weights("encoder.weights.h5")
-dec.load_weights("decoder.weights.h5")
+enc.load_weights("encoder_50000.weights.h5")
+dec.load_weights("decoder_50000.weights.h5")
 
 # Unit test for model loading
 z_sample = tf.random.normal(shape=(1, latent_dim))
@@ -442,61 +356,3 @@ plt.imshow(img_np)
 plt.axis('off')
 plt.show()
 
-'''
-
-z_mean, _, _, _ = encoder.predict([x_test, y_test], batch_size=32)
-
-import numpy as np
-
-nan_rows = np.any(np.isnan(z_mean), axis=1)
-print(f"Nb of samples containing NaN : {np.sum(nan_rows)} / {len(z_mean)}")
-
-print(z_mean[:1])
-
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-# z_fixed = np.nan_to_num(z_mean, nan=0.0)  # replace all NaN by 0.0
-
-
-# t-SNE (for decreasing the dimension, can also use PCA)
-z_2d = TSNE(n_components=2, perplexity=30, random_state=42).fit_transform(z_mean)
-
-y_colormap = (y_test.flatten() + 1) // 2
-
-plt.figure(figsize=(10, 8))
-scatter = plt.scatter(z_2d[:, 0], z_2d[:, 1], c=y_colormap, cmap="coolwarm", alpha=0.7)
-plt.colorbar(scatter, ticks=[0, 1], label="Gender")
-plt.xlabel("z[0]")
-plt.ylabel("z[1]")
-plt.title("Latent Space Clustering by Gender")
-plt.grid(True)
-plt.show()
-
-"""# The gender clusters are not well seperated, but I tried to generate male/female images and it seems to be not bad..."""
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-
-# generate a random latent z
-z_sample = np.random.normal(size=(1, latent_dim))
-
-# condition label：-1 = female，1 = male
-label_male = np.array([[1]])
-label_female = np.array([[-1]])
-
-# generate male/female image
-img_male = decoder.predict([z_sample, label_male])
-img_female = decoder.predict([z_sample, label_female])
-
-plt.figure(figsize=(4, 2))
-plt.subplot(1, 2, 1)
-plt.title("Male")
-plt.imshow(img_male[0])
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.title("Female")
-plt.imshow(img_female[0])
-plt.axis('off')
-plt.show()
-"""
